2521ecab70ec001facb742cdaf1b970647b82551
Kurniawan Satria <satganzdevs@gmail.com>
2024-10-12 17:36:28 +0700

    Update router.js

--- src/routes/router.js
+++ src/routes/router.js
@@ -18,31 +18,31 @@ router.get((req, res, next) => {
 router.get("/", (req, res) =>
   res.send(`<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="description" content="SwaggerUI" />
-    <title>SwaggerUI</title>
-    <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui.css" />
-  </head>
-  <body>
-  <div id="swagger-ui"></div>
-  <script src="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui-bundle.js" crossorigin></script>
-  <script src="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui-standalone-preset.js" crossorigin></script>
-  <script>
-    window.onload = () => {
-      window.ui = SwaggerUIBundle({
-        url: 'https://raw.githubusercontent.com/SatganzDevs/muslim-api/refs/heads/main/docs.json',
-        dom_id: '#swagger-ui',
-        presets: [
-          SwaggerUIBundle.presets.apis,
-          SwaggerUIStandalonePreset
-        ],
-        layout: "StandaloneLayout",
-      });
-    };
-  </script>
-  </body>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Islamic API Documentation</title>
+<meta name="description" content="Dokumentasi API untuk Quran, Doa, Dzikir, dan Hadits. Tersedia untuk digunakan dalam aplikasi WhatsApp Bot.">
+<meta name="author" content="Islamic Dev Team">
+<meta property="og:image" content="https://www.systemoflife.com/wp-content/uploads/2018/09/favicon.png">
+<link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@3.52.1/swagger-ui.css">
+<link rel="icon" type="image/x-icon" href="https://www.systemoflife.com/wp-content/uploads/2018/09/favicon.png">
+</head>
+<body>
+<div id="swagger-ui"></div>
+<script src="https://unpkg.com/swagger-ui-dist@3.52.1/swagger-ui-bundle.js"></script>
+<script src="https://unpkg.com/swagger-ui-dist@3.52.1/swagger-ui-standalone-preset.js"></script>
+<script>
+window.onload = function() {
+const ui = SwaggerUIBundle({
+url: "../../docs.json",
+dom_id: '#swagger-ui',
+presets: [SwaggerUIBundle.presets.apis],
+layout: "BaseLayout"
+});
+}
+</script>
+</body>
 </html>`)
 );
 
